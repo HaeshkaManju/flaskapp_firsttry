39d410a4ba86a47bf78ef5fe1b587cb9a6801bb8
C. Michael Fisher <Haeshka@gmail.com>
2018-08-16 10:15:29 -0600

    adding the database
    added the database - sqlite 'site.db'

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,17 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+
+######################################################################
+# App configurations.  Duh.
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '4038aa7c4030dc7c502bf6847351f6ff'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# Need to learn about SQALCHEMY Track modifications.
+db = SQLAlchemy(app)
 
+######################################################################
 ## home_title = 'Home'
 about_title = 'About'
 posts = [
@@ -20,6 +28,32 @@ posts = [
         'date_posted': 'April 21, 2018'
     }
 ]
+
+######################################################################
+# Models
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+######################################################################
 
 @app.route("/")
 @app.route("/home")
@@ -47,7 +81,7 @@ def login():
             flash('You have been logged into the system.', 'success')
             return redirect(url_for('home'))
         else:
-            flash('No, failed login.', 'danger')
+            flash('Login insuccessful.  Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
